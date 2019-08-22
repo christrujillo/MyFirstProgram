164e4c19add27371c86aef935258c0d4e31a4c2a
Christopher Trujillo <crisgmrl55@gmail.com>
2019-08-21 17:47:28 -0700

    MyFirstProgram

--- Program.cs
+++ Program.cs
@@ -1,12 +1,54 @@
-﻿using System;
+﻿/// Chapter No. 1		Exercise No. 1
+/// File Name: MyFirstProgram.java
+/// @author: Bill Gates
+/// Date:  August 24, 2015
+///
+/// Problem Statement: Ask the user to enter two numbers, calculate the sum of
+/// these two numbers, and display the sum to the screen
+/// 
+/// 
+/// Overall Plan:
+/// 1) Print an initial welcoming message to the screen
+/// 2) Prompt the user for two integers
+/// 3) Calculate the sum of the integers
+/// 4) Print the sum of the integers to the screen
+/// 
 
-namespace MyFirstProgram
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
 {
-    class Program
+    class MyFirstProgram
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // print a message to the screen
+            Console.WriteLine("Hello out there.");
+            Console.WriteLine("I will add two numbers for you.");
+            Console.WriteLine("Enter one number and press enter, then enter second number and press enter:");
+
+            // declare two integer variables
+            int n1, n2, sum;
+
+            string userInput;
+            userInput = Console.ReadLine();
+            n1 = Int32.Parse(userInput);
+            n2 = Int32.Parse(Console.ReadLine());
+
+            // calculate the sum of these two numbers
+            sum = n1 + n2;
+
+            // print a message along with the sum of the two numbers
+            Console.WriteLine("The sum of those two numbers is");
+            Console.WriteLine(sum);
+
+            //Just to pause the screen.
+            Console.ReadLine();
+
         }
     }
 }
